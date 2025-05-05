6b4e017a3f2ef11e847023557d65f367461600c7
Alejo Garrido <alejo.garrido0@gmail.com>
2025-05-05 19:12:10 -0300

    #Avance-crud

---
+++ src/Obra Social/obra-social.entity.ts
@@ -0,0 +1,14 @@
+import { publicDecrypt } from "crypto"
+
+
+export class Medical_Insurance {
+    public id_obra_social = crypto.randomUUID();
+    public _name: string;
+
+
+    constructor(_name: string){
+        this._name = _name;
+        this.id_obra_social = crypto.randomUUID();
+    }
+
+}
---
+++ src/Obra Social/obra-social.repository.ts
@@ -0,0 +1,13 @@
+import { publicDecrypt } from "crypto";
+import { Repository } from "../Shared/repository.js";
+import { Medical_Insurance} from "./obra-social.entity.js";
+
+const Medical_Insurance_Const  = [
+    new Medical_Insurance(
+        'Karin Traid',
+    )
+]
+
+//export class pacienteRepository implements Repository<Paciente> {
+    //Implementar todos los metodos de la interface
+//}
