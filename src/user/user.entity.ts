e8b6c32b03a1075eb216870f27e38c6329bd6a06
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-05-12 18:32:50 -0300

    Se corrige la ubicación de User

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,5 @@
 import { Entity } from '@mikro-orm/core';
-import { BaseEntity } from '../base.entity.js';
+import { BaseEntity } from '../shared/base.entity.js';
 
 @Entity()
 export class User extends BaseEntity {}
