c99e8c8878c8c3a3aa486597fc248dae0a1ee1a0
JSantos <juanpisantos2010@gmail.com>
2025-05-13 23:04:22 -0300

    #Add relaciones AppointmentStatus/TypeAppointmentStatus

--- src/appointmentStatus/appointmentStatus.entity.ts
+++ src/appointmentStatus/appointmentStatus.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, OneToMany, ManyToMany, Property, Cascade, Collection } from '@mikro-orm/core';
+import { Entity, OneToMany, ManyToMany, Property, Cascade, Collection, ManyToOne } from '@mikro-orm/core';
 import { BaseEntity } from '../shared/base.entity.js';
+import { TypeAppointmentStatus } from '../typeAppointmentStatus/typeAppointmentStatus.entity.js';
 
 @Entity()
 export class AppointmentStatus extends BaseEntity {
@@ -8,4 +9,15 @@ export class AppointmentStatus extends BaseEntity {
 
   @Property({ nullable: false })
   observations!: string;
+
+  @ManyToOne( ()  => TypeAppointmentStatus)
+  typeAppointmentStatus: TypeAppointmentStatus;
+
+  constructor( date: string, observations: string, typeAppoitmentStatus: TypeAppointmentStatus){
+    super();
+    this.date = date;
+    this.observations = observations;
+    this.typeAppointmentStatus = typeAppoitmentStatus;
+  }
+
 }
--- src/typeAppointmentStatus/typeAppointmentStatus.entity.ts
+++ src/typeAppointmentStatus/typeAppointmentStatus.entity.ts
@@ -1,8 +1,12 @@
 import { Entity, OneToMany, ManyToMany, Property, Cascade, Collection } from '@mikro-orm/core';
 import { BaseEntity } from '../shared/base.entity.js';
+import { AppointmentStatus } from '../appointmentStatus/appointmentStatus.entity.js';
 
 @Entity()
 export class TypeAppointmentStatus extends BaseEntity {
   @Property({ nullable: false })
   name!: string;
+
+  @OneToMany( () => AppointmentStatus, ap => ap.typeAppointmentStatus)
+  appointmentStatus = new Collection<AppointmentStatus>(this)
 }
