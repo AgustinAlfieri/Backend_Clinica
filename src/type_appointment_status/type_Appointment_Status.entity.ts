6279bfea9131d7ced39be1c32870ac8bf24df1e2
Toloza Laureano <laublake213@gmail.com>
2025-05-13 21:42:47 -0300

    Corrección de nombres de archivos

--- src/type_appointment_status/type_Appointment_Status.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, OneToMany, ManyToMany, Property, Cascade, Collection } from '@mikro-orm/core';
-import { BaseEntity } from '../shared/base.entity.js';
-
-@Entity()
-export class TypeAppointmentStatus extends BaseEntity {
-  @Property({ nullable: false })
-  name!: string;
-}
