90eab797bd8330bee88555e0b85d38af4629cad7
JSantos <juanpisantos2010@gmail.com>
2025-05-14 21:22:22 -0300

    #Ajuste

--- src/user/userTypes/patient/patient.entity.ts
+++ src/user/userTypes/patient/patient.entity.ts
@@ -1,7 +1,7 @@
-import { Entity, ManyToOne, PrimaryKey, Rel, OneToMany, Cascade, Collection } from '@mikro-orm/core';
-import { User } from '../../user.entity.js';
+import { Entity, ManyToOne, PrimaryKey, Rel, OneToMany, Cascade, Collection } from '@mikro-orm/core'
 import { MedicalInsurance } from '../../../medicalInsurance/medicalinsurance.entity.js';
 import { Appointment } from '../../../appointment/appointment.entity.js';
+import { User } from '../../user.entity.js';
 
 @Entity()
 export class Patient extends User {
