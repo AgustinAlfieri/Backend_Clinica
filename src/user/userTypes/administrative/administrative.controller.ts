b91d57d0dc011d3ea9ed6a2bc779d0004ed26d5e
Alejo Garrido <alejo.garrido0@gmail.com>
2025-08-18 18:40:22 -0300

    Corrección CRUD-Administrativo

--- src/user/userTypes/administrative/administrative.controller.ts
+++ src/user/userTypes/administrative/administrative.controller.ts
@@ -26,48 +26,32 @@ function sanitizeInputAdmin(req: Request, res: Response, next: NextFunction) {
 }
 
 async function findAll(req: Request, res: Response) {
-    em.find(Administrative, {}, { populate: ['appointments'] })
-      .then((administratives) => {
-          if (!administratives.length) throw new AppError('No se encontraron administrativos', StatusCodes.NOT_FOUND);
-          res.status(StatusCodes.ACCEPTED).send({ message: 'Administrativos encontrados: ', data: administratives });
-        })
-        .catch(() => {
-          logger.error('Error findAll');
-          throw new AppError('Error en busqueda', StatusCodes.INTERNAL_SERVER_ERROR);
-        });
-    }
+  const administratives = await em.find(Administrative, {}, { populate: ['appointments'] })
+  
+  if (!administratives) throw new AppError('No se encontraron administrativos', StatusCodes.NOT_FOUND);
+  
+  res.status(StatusCodes.ACCEPTED).send({ message: 'Administrativos encontrados: ', data: administratives });
+}
 
 async function findOne(req: Request, res: Response) {
-  em.findOne(Administrative, req.params.id, { populate: ['appointments'] })
-    .then((administrative) => {
-      if (!administrative) throw new AppError('Medico no encontrado', StatusCodes.NOT_FOUND);
-      res.status(StatusCodes.ACCEPTED).send({ message: 'Medico encontrado: ', data: administrative });
-    })
-    .catch(() => {
-      throw new AppError('Error en busqueda', StatusCodes.INTERNAL_SERVER_ERROR);
-    });
+  const id = req.params.id
+  const administrative = await em.findOne(Administrative, id , { populate: ['appointments'] })
+  
+  if (!administrative) throw new AppError('Adminitrativo no encontrado', StatusCodes.NOT_FOUND);
+  
+  res.status(StatusCodes.ACCEPTED).send({ message: 'Administrativo encontrado: ', data: administrative });
 }
 
 async function create(req: Request, res: Response) {
-  const { dni, name, email, password, telephone } = req.body.sanitizedInput;
-
-  const administrative = em.create(Administrative, {
-    dni,
-    name,
-    email,
-    password,
-    telephone,
-  });
-
-  await em
-    .flush()
-    .then(() => {
-      res.status(StatusCodes.CREATED).send(administrative);
-    })
-    .catch(() => {
-      throw new AppError('Error en busqueda', StatusCodes.INTERNAL_SERVER_ERROR);
-    });
+  try {
+    const administrative1 = req.body.sanitizedInput;
+    const administrative = em.create(Administrative, administrative1);
+    await em.persistAndFlush(administrative); // <-- Cambia esto
+    res.status(StatusCodes.CREATED).send(administrative);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "No se ha creado el usuario administrativo" });
   }
+}
 
 async function update(req: Request, res: Response) {
   const id: string = req.params.id; 
