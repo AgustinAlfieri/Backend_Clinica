73f0ef8c37ba60e2373f99c56d652c79b4716040
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-10-07 19:01:49 -0300

    borro path origin cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import "reflect-metadata";
-import express from "express";
-import cors from "cors";
-import { orm, syncSchema } from "./shared/database/orm.js";
-import { RequestContext } from "@mikro-orm/core";
-import { errorHandler } from "./shared/middlewares/errorHandler.js";
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+import { orm, syncSchema } from './shared/database/orm.js';
+import { RequestContext } from '@mikro-orm/core';
+import { errorHandler } from './shared/middlewares/errorHandler.js';
 
 const app = express();
 app.use(express.json());
@@ -11,10 +11,10 @@ app.use(express.json());
 // 👉 CORS primero (antes que MikroORM y rutas)
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend de Vite
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // si vas a mandar cookies/sesiones
+    origin: '', // frontend de Vite
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // si vas a mandar cookies/sesiones
   })
 );
 
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 const initApp = async () => {
-  const routes = await import("./shared/routes.js");
+  const routes = await import('./shared/routes.js');
   routes.default(app); // tus rutas
   app.use(errorHandler); // error handler al final
 };
@@ -33,5 +33,5 @@ await syncSchema(); // ⚠️ solo en dev, nunca en producción
 await initApp();
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log('Server running on http://localhost:3000');
 });
