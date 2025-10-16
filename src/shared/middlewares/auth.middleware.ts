e2094c89d9bbde545c55be9de2fcb737a3ffbddc
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-10-16 14:54:21 -0300

    Ajusto el auth para que tome la entidad del body de la peticion y no del rol del usuario

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -2,28 +2,33 @@ import { NextFunction, Request, Response } from 'express';
 import { verifyToken } from '../auth/auth.service.js';
 import { checkPermissionMiddleware } from './checkPermissionMiddleware.js';
 
-interface decodeData { userId: string; role: string; }
+interface decodeData {
+  userId: string;
+  role: string;
+}
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   try {
     //Agarro header
     const authHeader = req.headers.authorization;
-    if (!authHeader) { res.status(401).json({ message: 'No autorizado' });  return;}
+    if (!authHeader) {
+      res.status(401).json({ message: 'No autorizado' });
+      return;
+    }
 
     //saco token y verifico
-    const token =  authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1];
     const decoded = verifyToken(token);
 
     //Agrego info del usuario al request
     (req as any).user = decoded as decodeData;
 
     //Chequeo dentro de authMiddleware - Ajuste por consulta con JA
-    const entity = decoded.role;
+    const entity = req.body.entity || '';
     const action = req.body.action || '';
 
     checkPermissionMiddleware(entity, action);
     next();
-
   } catch (error) {
     res.status(401).json({ message: 'No autorizado' });
   }
